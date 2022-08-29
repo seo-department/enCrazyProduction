308b6e129d1cd898b0350570f8465672b7878f1b
seo-department <akani@goldmedialab.com>
2022-08-29 14:10:04 +0200

    Latest commit for redirection

--- src/app/pages/android-casinos/android-casinos.component.ts
+++ src/app/pages/android-casinos/android-casinos.component.ts
@@ -11,7 +11,7 @@ export class AndroidCasinosComponent implements OnInit {
   constructor(private metaService: Meta, private metaTitle: Title) { }
 
   ngOnInit(): void {
-    this.metaTitle.setTitle('Android Casinos – Top 10 Real Money Android Apps');
+    this.metaTitle.setTitle('Best Android Casinos – Top 10 Real Money Android Apps');
     this.metaService.addTags([
       {name: 'description', content: 'Looking for the top 10 Android Casinos? Android casino apps offer safe mobile casino gaming plus the up to $3000 in Andriod bonuses.'},
       { name: 'keywords', content: 'Android Casinos' },
