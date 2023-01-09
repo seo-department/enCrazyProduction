f96ef74093fee80f74e2bb84d5a53981d5d0f04d
seo-department <54539957+seo-department@users.noreply.github.com>
2023-01-09 16:27:39 +0200

    Update year to 2023

--- src/app/pages/banking/debit-card-casinos/debit-card-casinos.component.ts
+++ src/app/pages/banking/debit-card-casinos/debit-card-casinos.component.ts
@@ -11,7 +11,7 @@ export class DebitCardCasinosComponent implements OnInit {
   constructor(private metaService: Meta, private metaTitle: Title) { }
 
   ngOnInit(): void {
-    this.metaTitle.setTitle('Best Debit Card Casinos 2022 - Casinos Accepting Debit Cards');
+    this.metaTitle.setTitle('Best Debit Card Casinos 2023 - Casinos Accepting Debit Cards');
     this.metaService.addTags([
       {name: 'description', content: 'Debit Card casinos transaction method. Find the top online casinos accepting debit cards such as VISA to make quick and easy deposits & withdrawals.'},
       { name: 'keywords', content: 'Best Debit Card Casinos' },
